0ef3576a5edb3de8aaa4fb785c15af9142c8ebbf
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2024-02-21 21:24:34 +0800

    added user management forms

--- apps/tradein-admin/src/app/pages/user-management/index.tsx
+++ apps/tradein-admin/src/app/pages/user-management/index.tsx
@@ -1,14 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import {
+  ACTIONS_COLUMN,
   AppButton,
   DEFAULT_COLUMN,
+  SideModal,
   Table,
   USER_MANAGEMENT_COLUMNS,
   useCommon,
+  useCustomEffect,
   useUser,
 } from '@tradein-admin/libs';
-import { useEffect, useRef } from 'react';
+import { useState } from 'react';
+import { AddUserForm } from './add-user';
+import { EditUserForm } from './edit-user';
 
 export function UserManagementPage() {
   const { state, getUsers, clearUsers } = useUser();
@@ -16,15 +22,16 @@ export function UserManagementPage() {
   const { state: commonState, setSideModalState } = useCommon();
   const { sideModalState } = commonState;
 
-  const shouldRun = useRef(true);
+  const headers = [
+    ...DEFAULT_COLUMN,
+    ...USER_MANAGEMENT_COLUMNS,
+    ...ACTIONS_COLUMN,
+  ];
 
-  const headers = [...DEFAULT_COLUMN, ...USER_MANAGEMENT_COLUMNS];
+  const [selectedUser, setSelectedUser] = useState({});
 
-  useEffect(() => {
-    if (shouldRun.current) {
-      getUsers({});
-      shouldRun.current = false;
-    }
+  useCustomEffect(() => {
+    getUsers({});
 
     return () => {
       // Clear data on unmount
@@ -32,21 +39,63 @@ export function UserManagementPage() {
     };
   }, []);
 
+  const renderForm = () => {
+    switch (sideModalState.view) {
+      case 'add-user':
+        return <AddUserForm />;
+
+      case 'edit-user':
+        return <EditUserForm data={selectedUser} />;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <Table
-      label="Users"
-      isLoading={isFetchingUsers}
-      headers={headers}
-      rows={users || []}
-      rightControls={
-        <AppButton
-          width="fit-content"
-          icon={faPlus}
-          onClick={() => setSideModalState({ ...sideModalState, open: true })}
-        >
-          Add
-        </AppButton>
-      }
-    />
+    <>
+      <Table
+        label="Users"
+        isLoading={isFetchingUsers}
+        headers={headers}
+        rows={users || []}
+        menuItems={[
+          {
+            label: 'Edit',
+            action: (value: any) => {
+              setSelectedUser(value);
+              setSideModalState({
+                ...sideModalState,
+                open: true,
+                view: 'edit-user',
+              });
+            },
+          },
+        ]}
+        rightControls={
+          <AppButton
+            width="fit-content"
+            icon={faPlus}
+            onClick={() => {
+              setSideModalState({
+                ...sideModalState,
+                open: true,
+                view: 'add-user',
+              });
+            }}
+          >
+            Add
+          </AppButton>
+        }
+      />
+      <SideModal
+        isOpen={sideModalState?.open}
+        onClose={() => {
+          setSideModalState({ ...sideModalState, open: false, view: null });
+        }}
+      >
+        {renderForm()}
+      </SideModal>
+    </>
   );
 }
