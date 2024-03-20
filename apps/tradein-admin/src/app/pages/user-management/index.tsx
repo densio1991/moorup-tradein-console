da0e99ee8f8462fa903b2e0f623699ece0147325
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2024-03-20 19:50:04 +0800

    temporarily disable add user button

--- apps/tradein-admin/src/app/pages/user-management/index.tsx
+++ apps/tradein-admin/src/app/pages/user-management/index.tsx
@@ -100,6 +100,7 @@ export function UserManagementPage() {
                 view: MODAL_TYPES.ADD_USER,
               });
             }}
+            disabled
           >
             Add
           </AppButton>
