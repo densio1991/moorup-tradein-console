eabe33bed4de3c4570f9fddc315eaf2be7eeb286
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2024-04-12 03:50:58 +0800

    added rows per page action

--- libs/src/constants/constants.tsx
+++ libs/src/constants/constants.tsx
@@ -1295,3 +1295,22 @@ export const MOORUP_CLAIM_STATUSES = [
   { value: ClaimStatus.PENDING, label: 'Pending' },
   { value: ClaimStatus.REJECTED, label: 'Rejected' },
 ]
+
+export const PAGE_SIZES = [
+  {
+    label: '10',
+    value: '10',
+  },
+  {
+    label: '25',
+    value: '25',
+  },
+  {
+    label: '50',
+    value: '50',
+  },
+  {
+    label: '100',
+    value: '100',
+  }
+];
