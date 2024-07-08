5cfb69495af87f48d9e52cfbaab61e782d1bf0c4
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2024-07-09 01:22:44 +0800

    added styled link component and updated others

--- libs/src/components/page-subheader/page-subheader.tsx
+++ libs/src/components/page-subheader/page-subheader.tsx
@@ -3,7 +3,15 @@ import styled from 'styled-components';
 import { useCommon } from '../../store';
 import { StyledInput } from '../input';
 
-const StyledContainer = styled.div<{ overflowx?: string; overflowy?: string; }>`
+const StyledContainer = styled.div<{ 
+  overflowx?: string; 
+  overflowy?: string; 
+  margin?: string;
+  marginTop?: string;
+  marginBottom?: string;
+  marginLeft?: string;
+  marginRight?: string;
+}>`
   height: 54px;
   width: calc(100% - 40px);
   padding: 4px 20px;
@@ -19,6 +27,11 @@ const StyledContainer = styled.div<{ overflowx?: string; overflowy?: string; }>`
   z-index: 888;
   ${(props) => props.overflowx && `overflow-x: ${props.overflowx};`}
   ${(props) => props.overflowy && `overflow-y: ${props.overflowy};`}
+  ${(props) => props.margin && `margin: ${props.margin};`}
+  ${(props) => props.marginTop && `margin-top: ${props.marginTop};`}
+  ${(props) => props.marginBottom && `margin-bottom: ${props.marginBottom};`}
+  ${(props) => props.marginLeft && `margin-left: ${props.marginLeft};`}
+  ${(props) => props.marginRight && `margin-right: ${props.marginRight};`}
 `;
 
 const LeftSection = styled.div`
@@ -41,15 +54,39 @@ interface PageSubHeaderProps {
   withSearch?: boolean;
   overflowx?: string;
   overflowy?: string;
+  margin?: string;
+  marginTop?: string;
+  marginBottom?: string;
+  marginLeft?: string;
+  marginRight?: string;
 }
 
-export function PageSubHeader({ leftControls, rightControls, withSearch, overflowx, overflowy }: PageSubHeaderProps) {
+export function PageSubHeader({ 
+  leftControls, 
+  rightControls, 
+  withSearch, 
+  overflowx, 
+  overflowy,
+  margin,
+  marginTop,
+  marginBottom,
+  marginLeft,
+  marginRight,
+}: PageSubHeaderProps) {
   const { state: commonState, setSearchTerm } = useCommon();
   const { searchTerm } = commonState;
 
   return (
     <div className="card">
-      <StyledContainer overflowx={overflowx} overflowy={overflowy}>
+      <StyledContainer 
+        overflowx={overflowx} 
+        overflowy={overflowy}
+        margin={margin}
+        marginTop={marginTop}
+        marginBottom={marginBottom}
+        marginLeft={marginLeft}
+        marginRight={marginRight}
+      >
         <LeftSection>
           {leftControls}
         </LeftSection>
---
+++ libs/src/components/styled/link.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledLink = styled.a`
+  text-decoration: underline;
+  color: #216A4C;
+  display: inline-block;
+  max-width: 300px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: bottom;
+`;
